4adf89ca5ff5c6c54f45caaeeeffa913b2c32ea4
Ivan Sim <ihcsim@gmail.com>
2014-09-02 02:24:37 +0000

    Add celsius-to-fahnrenheit conversion. Round to 2 decimal places.

--- src/module/convert/temperature.rb
+++ src/module/convert/temperature.rb
@@ -2,7 +2,12 @@ module Convert
   class Temperature
     def self.fahrenheit_to_celsius(input)
       raise ArgumentError, "Argument must be numeric\n" unless input.is_a? Numeric
-      return Float((input - 32))/1.8000
+      return (Float((input - 32))/1.8000).round(2)
+    end
+
+    def self.calsius_to_fahrenheit(input)
+      raise ArgumentError, "Argument must be numeric\n" unless input.is_a? Numeric
+      return (Float(input * 1.80000) + 32.00).round(2)
     end
   end
 end
