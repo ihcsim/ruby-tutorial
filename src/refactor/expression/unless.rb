9a95b2c0f00a6d3913e16edf2685418e44a39034
Ivan Sim <ihcsim@gmail.com>
2014-09-06 22:48:10 +0000

    Add refactor codes with unit tests.

---
+++ src/refactor/expression/unless.rb
@@ -0,0 +1,27 @@
+# Ensure that games_count and games_count_refactor exhibit the same behaviour.
+# To run, execute $ruby unless.rb
+
+require 'minitest/autorun'
+
+class TestUnless < MiniTest::Unit::TestCase
+  def setup
+    @games = ["Super Mario Bros.", "Contra", "Metroid", "Mega Man 2"]
+  end
+
+  def test_games_count    
+    assert_equal(4, games_count)
+    assert_equal(4, games_count_refactor)
+  end
+
+  def games_count
+    if !@games.empty?
+      @games.count
+    end
+  end
+
+  def games_count_refactor
+    unless @games.empty?
+      @games.count
+    end
+  end
+end
