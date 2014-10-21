a230af673624b455a5661d2478cea7600366174e
Ivan Sim <ihcsim@gmail.com>
2014-10-21 03:08:20 +0000

    Replace zombie instantiation with 'subject'.

--- spec/lib/zombie_spec.rb
+++ spec/lib/zombie_spec.rb
@@ -4,23 +4,19 @@ require "./zombie"
 
 describe Zombie do
   it "is named Ash" do
-    zombie = Zombie.new
-    expect(zombie.name).to eq 'Ash'
+    expect(subject.name).to eq 'Ash'
   end
 
   it "has no brains" do
-    zombie = Zombie.new
-    expect(zombie.brain).to be < 1
+    expect(subject.brain).to be < 1
   end
 
   it "is hungry" do
-    zombie = Zombie.new
-    expect(zombie).to be_hungry
+    expect(subject).to be_hungry
   end
 
   it 'is invalid without a name' do
-    zombie = Zombie.new
-    expect(zombie).not_to be_valid
+    expect(subject).not_to be_valid
   end
 
   it 'include tweets' do
@@ -32,10 +28,6 @@ describe Zombie do
   end
 
   it 'starts with two weapons' do
-    zombie = Zombie.new(name: 'Ash')
-    expect(zombie).to have(2).weapons
-
+    expect(subject).to have(2).weapons
   end
-
-
 end
